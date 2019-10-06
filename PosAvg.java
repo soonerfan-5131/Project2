1ed2e87676b09339ae3f8feafa9092dce3edb4d2
Ryan <Ryan.G.Wesson-1@ou.edu>
2019-10-06 14:01:13 -0500

    added indexOfStation method

--- PosAvg.java
+++ PosAvg.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 
 public class PosAvg {
 	private String str;
+	ArrayList<String> file = new ArrayList<String>();
 	//Default constructor
 	public PosAvg() {
 		
@@ -8,5 +10,17 @@ public class PosAvg {
 	//PosAvg constructor
 	public PosAvg(String str) {
 		this.str = str;
+	}
+	//Find the index of the station
+	public int indexOfStation() {
+		String temp = "";
+		int statIndex = 0;
+		for(int i = 0; i < file.size(); i++) {
+			temp = file.get(i);
+			if(str.equals(temp)) {
+				statIndex = file.indexOf(temp);
+			}
+		}
+		return statIndex;
 	}
 }
