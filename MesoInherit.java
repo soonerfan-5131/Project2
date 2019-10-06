e12a02f69b6ab1c4c8be14694d4a4f0df18a3842
Ryan <Ryan.G.Wesson-1@ou.edu>
2019-10-06 13:39:11 -0500

    extended MesoInherit and added methods with stubs for now

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,4 +1,11 @@
 
-public class MesoInherit {
-
+public class MesoInherit extends MesoAbstract{
+	public int[] calAverage() {
+		return null;//stub
+	}
+	public char letterAverage() {
+		char a = 'a';//stub
+		return a;//stub
+		
+	}
 }
