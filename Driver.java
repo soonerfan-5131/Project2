94b7b2a11659bf55ddaff167cd503d67f49d77ba
Ryan <Ryan.G.Wesson-1@ou.edu>
2019-10-10 13:56:35 -0500

    completed methods in MesoInherit, tested in Driver

--- Driver.java
+++ Driver.java
@@ -9,10 +9,10 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException 
 	{
+		String stID = "NRMN";
+		PosAvg p = new PosAvg(stID);
 		//p.indexOfStation();
 		//p.showArray();
-		String stID = "OKCE";
-		//PosAvg p = new PosAvg(stID);
 		//p.readFile("Mesonet.txt");
 		//p.indexOfStation();
 		MesoInherit mesoInherit = new MesoInherit(new MesoStation(stID));
@@ -23,16 +23,16 @@ public class Driver
 		System.out.println(posAvg);
 		System.out.print("\n");
 		
-		//System.out.println("Ascii Ceiling is " + mesoInherit.calAverage()[0]);
-		//System.out.println("Ascii Floor is " + mesoInherit.calAverage()[1]);
-		//System.out.println("Ascii Average is " + mesoInherit.calAverage()[2]);
-		//System.out.print("\n");
+		System.out.println("Ascii Ceiling is " + mesoInherit.calAverage()[0]);
+		System.out.println("Ascii Floor is " + mesoInherit.calAverage()[1]);
+		System.out.println("Ascii Average is " + mesoInherit.calAverage()[2]);
+		System.out.print("\n");
 		
-		//System.out.println("Letter Avg: " + mesoInherit.letterAverage());
-		//System.out.print("\n");
+		System.out.println("Letter Avg: " + mesoInherit.letterAverage());
+		System.out.print("\n");
 		
-		//LetterAvg letterAvg = new LetterAvg(mesoInherit.letterAverage());
-		//System.out.println("Total number of stations starting with letter '" + mesoInherit.letterAverage() + "' is " + letterAvg.numberOfStationWithLetterAvg() + ".");
+		LetterAvg letterAvg = new LetterAvg(mesoInherit.letterAverage());
+		System.out.println("Total number of stations starting with letter '" + mesoInherit.letterAverage() + "' is " + letterAvg.numberOfStationWithLetterAvg() + ".");
 		
 		//System.out.print(letterAvg);
 	}
--- MesoInherit.java
+++ MesoInherit.java
@@ -16,20 +16,33 @@ public class MesoInherit extends MesoAbstract{
 	//MesoInherit constructor
 	public MesoInherit(MesoStation statID) {
 		this.statID = statID;
+		statName = statID.getStID();
 	}
 	public int[] calAverage() {
 		//Help with this
 		lettersAvg = new int[3];
 		char a = statName.charAt(0);
+		char b = statName.charAt(1);
+		char c = statName.charAt(2);
+		char d = statName.charAt(3);
 		double charAvg;
 		double ceiling;
 		double floor;
 		double avg;
+		double num = a;
+		double num2 = b;
+		double num3 = c;
+		double num4 = d;
+		charAvg = (num + num2 + num3 + num4)/4.0;
+		ceiling = Math.ceil(charAvg);
+		floor = Math.floor(charAvg);
+		avg = Math.rint(charAvg);
+		lettersAvg[0] = (int)ceiling;
+		lettersAvg[1] = (int)floor;
+		lettersAvg[2] = (int)avg;
 		return lettersAvg;
 	}
 	public char letterAverage() {
-		char a = 'a';//stub
-		return a;//stub
-		
+		return (char)calAverage()[2];
 	}
 }
